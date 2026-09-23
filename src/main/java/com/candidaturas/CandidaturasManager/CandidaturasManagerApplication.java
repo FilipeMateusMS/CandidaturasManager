eed550decdbf5007fb1e6f05aea164bee1217bc9
FilipeMateusMS <filipedesousademedeiros@gmail.com>
2026-09-23 09:49:53 -0300

    Arquivos iniciais

---
+++ src/main/java/com/candidaturas/CandidaturasManager/CandidaturasManagerApplication.java
@@ -0,0 +1,13 @@
+package com.candidaturas.CandidaturasManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidaturasManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidaturasManagerApplication.class, args);
+	}
+
+}
